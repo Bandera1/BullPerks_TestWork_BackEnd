1fde2f5fcc40e8b81ffcee1dda681a2b0074c9cb
Vladyslav Nehodiuk <johndoe@email.com>
2024-03-23 15:13:49 +0300

    Add project files.

---
+++ DB/EFDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BullPerks_TestWork.DB
+{
+    public class EFDbContext : IdentityDbContext<DbUser, IdentityRole, string, IdentityUserClaim<string>,
+    IdentityUserRole<string>, IdentityUserLogin<string>,
+    IdentityRoleClaim<string>, IdentityUserToken<string>>
+    {
+    }
+}
