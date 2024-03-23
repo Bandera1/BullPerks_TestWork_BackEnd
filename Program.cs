13c20a6e20af5d07c419d52afa867f5a2d02934f
Vladyslav Nehodiuk <johndoe@email.com>
2024-03-23 15:46:57 +0300

    Set up the database and db settings Created model Connect identity models and auth settings

--- Program.cs
+++ Program.cs
@@ -1,6 +1,51 @@
+using BullPerks_TestWork.DB;
+using BullPerks_TestWork.DB.IdentityModels;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
+#region DB
+builder.Services.AddDbContext<EFDbContext>(options =>
+                    options.UseSqlServer(connectionString));
+
+var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("a8f5f167f44f4964e6c998dee827110c"));
+builder.Services.AddIdentity<DbUser, IdentityRole>(options =>
+{
+    options.Stores.MaxLengthForKeys = 256;
+    options.Password.RequiredLength = 6;
+})
+    .AddEntityFrameworkStores<EFDbContext>()
+    .AddDefaultTokenProviders();
+
+#endregion
+
+#region Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(cfg =>
+{
+    cfg.RequireHttpsMetadata = false;
+    cfg.SaveToken = true;
+    cfg.TokenValidationParameters = new TokenValidationParameters()
+    {
+        IssuerSigningKey = signingKey,
+        ValidateAudience = false,
+        ValidateIssuer = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        // set ClockSkew is zero
+        ClockSkew = TimeSpan.Zero
+    };
+});
+#endregion
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
