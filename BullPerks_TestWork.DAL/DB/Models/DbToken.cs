50b364a11f9a069eb2244c073f17bf79431e8b38
Vladyslav Nehodiuk <pearmadn@email.com>
2024-03-25 03:06:37 +0300

    Folder fix

---
+++ BullPerks_TestWork.DAL/DB/Models/DbToken.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BullPerks_TestWork.Api.DB.Models
+{
+    public class DbToken
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public uint TotalSupply { get; set; }
+        public uint CirculatingSupply { get; set; }
+    }
+}
---
+++ BullPerks_TestWork.Domain/Models/JSON/BscScanGetTokenTotalSupplyModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BullPerks_TestWork.Api.Models.JSON
+{
+    public class BscScanGetTokenTotalSupplyModel
+    {
+        public string? Status { get; set; }
+        public string? Message { get; set; }
+        public string? Result { get; set; }
+    }
+}
